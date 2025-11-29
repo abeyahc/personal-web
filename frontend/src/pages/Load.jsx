a2d81cec7ac57f013722f16ac15c692ac62ed1e3
Abeyah Calpatura <abeyahc@gmail.com>
2025-11-29 03:20:32 -0800

    update animation

--- frontend/src/pages/Load.jsx
+++ frontend/src/pages/Load.jsx
@@ -4,8 +4,10 @@ import sprite from '../assets/personal-sprite.png';
 export default function Load() {
     return (
       <div className="flex justify-center items-center min-h-screen">
-         <div className="sprite scale-450" style={{backgroundImage: `url(${sprite})`}} />
+         <div 
+           className="sprite scale-450 sm:scale-200 md:scale-300 lg:scale-[4] xl:scale-[4.5] 2xl:scale-[5]" 
+           style={{backgroundImage: `url(${sprite})`}} 
+         />
       </div>
-     
     );
 }
