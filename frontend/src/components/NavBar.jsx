1e0d2595cad56cecfff51d455a684ba472f7ccbd
Abeyah Calpatura <abeyahc@gmail.com>
2025-12-04 01:17:22 -0800

    update my nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,40 +3,32 @@ import "nes.css/css/nes.min.css";
 
 export default function Navbar() {
   return (
-  <nav className="fixed left-10 top-10 bottom-10 w-48
-                flex flex-col h-screen
-                border-4 border-white
-                px-6 py-12
-                justify-start">
-      
+    <nav className="fixed left-0 top-0 min-h-screen w-[300px]
+                    flex flex-col justify-between
+                    bg-[#1d1d1d] border-r-[4px] border-white
+                    px-[32px] py-[48px] z-50">
+
       {/* Top Buttons */}
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-[32px] pt-[40px]">
         <Link to="/home">
-          <button className="nes-btn w-full is-normal">
-            About Me
-          </button>
+          <button className="nes-btn w-full is-normal">About Me</button>
         </Link>
 
         <Link to="/projects">
-          <button className="nes-btn w-full is-normal">
-            Projects
-          </button>
+          <button className="nes-btn w-full is-normal">Projects</button>
         </Link>
 
         <Link to="/photography">
-          <button className="nes-btn w-full is-normal">
-            Photography
-          </button>
+          <button className="nes-btn w-full is-normal">Photography</button>
         </Link>
       </div>
 
       {/* Bottom Social Icons */}
-      <div className="flex gap-3 items-center mt-auto pt-10">
+      <div className="flex gap-[15px] items-center justify-center mt-auto">
         <i className="nes-icon github is-medium"></i>
         <i className="nes-icon linkedin is-medium"></i>
         <i className="nes-icon instagram is-medium"></i>
       </div>
-
     </nav>
   );
 }
