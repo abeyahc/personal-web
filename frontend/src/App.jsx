ac2a0fce8341807562e25ea8ff2d5584229103c3
Abeyah Calpatura <abeyahc@gmail.com>
2025-12-04 01:39:50 -0800

    add links for projects and photography

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Load from "./pages/Load";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Photography from "./pages/Photography";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Load />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/projects" element={< Projects />} />
+      <Route path="/photography" element={< Photography />} />
     </Routes>
   );
 }
