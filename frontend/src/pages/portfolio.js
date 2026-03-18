b2c539b629c8eff767bdb662561620f606bcdaa6
Abeyah Calpatura <abeyahc@gmail.com>
2026-03-17 17:00:15 -0700

    added more photos to portfolio

--- frontend/src/pages/portfolio.js
+++ frontend/src/pages/portfolio.js
@@ -4,6 +4,7 @@ const portfolioImages = import.meta.glob("./portfolio/*.jpg", {
 });
 
 const photos = [
+  { src: portfolioImages["./portfolio/26A_00151.jpg"], width: 2400, height: 1600 },
   { src: portfolioImages["./portfolio/DSC04221.jpg"], width: 1690, height: 2000, description: "a photo of Ivana :)" },
   { src: portfolioImages["./portfolio/DSC04243.jpg"], width: 1333, height: 2000 },
   { src: portfolioImages["./portfolio/DSC04395.jpg"], width: 1333, height: 2000 },
@@ -26,6 +27,12 @@ const photos = [
   { src: portfolioImages["./portfolio/abstract1.jpg"], width: 1333, height: 2000 },
   { src: portfolioImages["./portfolio/abstract2.jpg"], width: 2000, height: 2000 },
   { src: portfolioImages["./portfolio/shadow4.jpg"], width: 2000, height: 1333 },
+  { src: portfolioImages["./portfolio/DSC05398.jpg"], width: 2000, height: 996, description: "on the way to Lake Tahoe", title: "2/28/26"},
+  { src: portfolioImages["./portfolio/DSC05457.jpg"], width: 1271, height: 2000, description: "Lake Tahoe with Friends", title: "2/28/26"},
+  { src: portfolioImages["./portfolio/DSC05611.jpg"], width: 2000, height: 1333, description: "pismo beach"},
+  { src: portfolioImages["./portfolio/DSC05683.jpg"], width: 1333, height: 2000, description: "cute bird at pismo beach"},
+  { src: portfolioImages["./portfolio/_25_00225.jpg"], width: 1600, height: 2400 ,title: "film", description: "botanical garden in seattle"   },
+
 ];
 
 export default [...photos].reverse();
